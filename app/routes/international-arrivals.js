904834edd4c8994dcfde538f80d97fa4a9f4e20a
Andrea Zavodska <azavodska@deloitte.co.uk>
2021-08-06 16:26:47 +0100

    update copy

--- app/routes/international-arrivals.js
+++ app/routes/international-arrivals.js
@@ -46,4 +46,76 @@ router.post('/international-arrivals/v1/action4/travel-details-same-person-1', f
   }
 })
 
+
+router.post('/international-arrivals/v2/action1/travel-route', function (req, res) {
+    let travelRoute = req.session.data['travel-route']
+    if (travelRoute == "red") {
+      res.redirect('/international-arrivals/v2/red-package-exit')
+    } else {
+      res.redirect('/international-arrivals/v2/personal-details')
+    }
+})
+
+router.post('/international-arrivals/v2/action2/contact-details-same-person-1', function (req, res) {
+  let contactDetailsSame = req.session.data['contact-details-same-person-1']
+  if (contactDetailsSame == "Yes") {
+    res.redirect('/international-arrivals/v2/check-your-answers-person-1')
+  } else {
+    res.redirect('/international-arrivals/v2/contact-details-person-1')
+  }
+})
+
+router.post('/international-arrivals/v2/action3/travel-route-same-person-1', function (req, res) {
+  let travelRouteSame = req.session.data['travel-route-same-person-1']
+  if (travelRouteSame == "No") {
+    res.redirect('/international-arrivals/v2/cannot-add-person')
+  } else {
+    res.redirect('/international-arrivals/v2/travel-details-same-person-1')
+  }
+})
+
+router.post('/international-arrivals/v2/action4/travel-details-same-person-1', function (req, res) {
+  let travelDetailsSame = req.session.data['travel-details-same-person-1']
+  if (travelDetailsSame == "No") {
+    res.redirect('/international-arrivals/v2/cannot-add-person')
+  } else {
+    res.redirect('/international-arrivals/v2/travel-route-same-person-1')
+  }
+})
+
+router.post('/international-arrivals/v2/action/vaccine-person-1', function (req, res) {
+  let vaccine = req.session.data['vaccine-person-1']
+  if (vaccine == "No"){
+    res.redirect('/international-arrivals/v2/previous-infection-person-1')
+  } else {
+    res.redirect('/international-arrivals/v2/vaccine-date-person-1')
+  }
+})
+
+router.post('/international-arrivals/v2/action/ethnic-group-person-1', function (req, res) {
+  let ethnicGroupPerson1 = req.session.data['ethnic-group-person-1']
+  if (ethnicGroupPerson1 == "Asian or Asian British"){
+    res.redirect('/international-arrivals/v2/ethnic-background-asian-person-1')
+  } else if (ethnicGroupPerson1 == "Black, African, Black British or Caribbean") {
+    res.redirect('/international-arrivals/v2/ethnic-background-black-person-1')
+  } else if (ethnicGroupPerson1 == "Mixed or multiple ethnic groups") {
+    res.redirect('/international-arrivals/v2/ethnic-background-mixed-person-1')
+  } else if (ethnicGroupPerson1 == "White") {
+    res.redirect('/international-arrivals/v2/ethnic-background-white-person-1')
+  } else if (ethnicGroupPerson1 == "Another ethnic group") {
+    res.redirect('/international-arrivals/v2/ethnic-background-another-person-1')
+  } else {
+    res.redirect('/international-arrivals/v2/currently-in-work-person-1')
+  }
+})
+
+router.post('/international-arrivals/v2/action/nhs-number-known-person-1', function (req, res) {
+  let nhsNumberKnown = req.session.data['nhs-number-known-person-1']
+  if (nhsNumberKnown == "Yes"){
+    res.redirect('/international-arrivals/v2/nhs-number-person-1')
+  } else {
+    res.redirect('/international-arrivals/v2/check-your-answers-person-1')
+  }
+})
+
 module.exports = router
